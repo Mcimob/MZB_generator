c603de089f6730bfc939600610bad7008951a902
mcimob <burckhardttim@gmail.com>
2023-05-23 19:32:45 +0200

    Put all into functions

---
+++ geo_admin_tools.py
@@ -0,0 +1,121 @@
+from pykml import parser
+from pykml.factory import nsmap
+import requests
+import json
+import openpyxl
+import matplotlib.pyplot as plt
+from wgs84_ch1903 import GPSConverter
+
+def create_marker(index, title, east, north):
+    return f"""<Placemark id="marker_{index + 1}">
+  <ExtendedData>
+    <Data name="type">
+      <value>marker</value>
+    </Data>
+  </ExtendedData>
+  <name>Marker {title}</name>
+  <description></description>
+  <Style>
+    <IconStyle>
+      <Icon>
+        <href>https://api3.geo.admin.ch/color/255,0,0/marker-24@2x.png</href>
+        <gx:w>48</gx:w>
+        <gx:h>48</gx:h>
+      </Icon>
+      <hotSpot x="24" y="4.799999999999997" xunits="pixels" yunits="pixels"/>
+    </IconStyle>
+    <LabelStyle>
+      <color>ff0000ff</color>
+    </LabelStyle>
+  </Style>
+  <Point>
+    <tessellate>1</tessellate>
+    <altitudeMode>clampToGround</altitudeMode>
+    <coordinates>{east},{north}</coordinates>
+  </Point>
+</Placemark>"""
+
+def addMarkersToKML(outFile, poi, template):
+    converter = GPSConverter()
+    markers = []
+    for index, p in enumerate(poi):
+        north, east, alt = (converter.LV03toWGS84(p['easting'], p['northing'], 0))
+        marker = create_marker(index, index, east, north)
+        markers.append(marker)
+        
+    
+    index = template.find('</Document>')
+    outFile.write(template[0:index])
+    for m in markers:
+        outFile.write(m)
+    outFile.write(template[index:-1])
+    return
+
+def kmlToLV03Coords(kml_text):
+  root = parser.fromstring(kml_text).getroot()
+  pms = root.xpath(".//ns:Placemark[.//ns:LineString]", namespaces={'ns': nsmap[None]})
+  coords = []
+  converter = GPSConverter()
+
+  for coord in pms[0].LineString.coordinates.text.split(' '):
+    xy = coord.split(',')
+    print(float(xy[0]), float(xy[1]))
+    coords.append(converter.WGS84toLV03(float(xy[1]), float(xy[0]), 0, clip=True)[0:2])
+    
+  return coords
+
+def getDetailedCoords(coords):
+  arg = json.dumps({"type": "LineString", "coordinates": coords})
+  return requests.get( f'https://api3.geo.admin.ch/rest/services/profile.json?geom={arg}&sr=21781&distinct_points=True', timeout=3, 
+    ).json()    
+
+def getRightAlts(coords):
+  for p in coords:
+      p['alt'] = p['alts']['DTM2']
+      del p['alts']
+
+def getPOI(coords):
+  for i in range(len(coords)):
+    coords[i]['relative'] = 'Start' if i==0 else 'End' if i==len(coords)-1 else 'High' if (coords[i-1]['alt'] < coords[i]['alt'] and coords[i]['alt'] > coords[i+1]['alt']) else 'Low' if (coords[i-1]['alt'] > coords[i]['alt'] and coords[i]['alt'] < coords[i+1]['alt']) else None
+      
+  poi_tmp = [c for c in coords if c['relative']]
+  poi = []
+  over = True
+  margin = 0
+  while over:
+    over = False
+    
+    for i in range(1, len(poi_tmp) - 1):
+      if max(abs(poi_tmp[i]['alt'] - poi_tmp[i-1]['alt']), abs(poi_tmp[i]['alt'] - poi_tmp[i+1]['alt'])) >= margin:
+        poi.append(poi_tmp[i])
+
+    poi.append(poi_tmp[-1])
+    poi.insert(0, poi_tmp[0])
+
+    if len(poi) > 21:
+      over = True
+      margin += 5
+      poi = []
+  return poi
+
+def writeToExcel(poi, filename):
+    book = openpyxl.load_workbook('MZB_template.xlsx')
+    sheet = book['leer']
+
+    for i, p in enumerate(poi):
+        sheet[f'A{i+8}'] = f'Point {i+1}'
+        sheet[f'C{i+8}'] = p['alt']
+        
+        if i != 0:
+            sheet[f'E{i+8}'] = (p['dist'] - p['dist'])/1000
+
+    book.save(filename)
+
+
+""" x = [p['dist'] for p in coords]
+y = [p['alt'] for p in coords]
+fig, ax = plt.subplots()
+
+ax.plot(x, y)
+
+plt.show() """
